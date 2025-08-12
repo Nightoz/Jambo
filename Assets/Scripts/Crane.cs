40a3e336ebb3ab3ff6cbb39a0c65fa8179cd2841
Nightoz <N1ghtFrog@yandex.ru>
2025-08-12 16:36:04 +0500

    кран

--- Assets/Scripts/Crane.cs
+++ Assets/Scripts/Crane.cs
@@ -6,11 +6,65 @@ using UnityEngine.Events;
 
 public class Crane : MonoBehaviour
 {
+    public float rotateSpeed = 5;
+    public Vector2 craneRotY = new(-11, 11);
+    public Vector2 craneRotX = new(0, 20);
+    [Header("Events")]
     public UnityEvent craneEnter;
-    public UnityEvent craneExit;
-
+    private Vector2 input;
+    private bool isCrane = false;
+    private float currentYRotation = 0f;
+    private float currentXRotation = 0f;
+    public Transform cranePivot;
+    public Transform joystic;
+    public Transform cran;
+    public Transform Gruz;
+    
+    
     private void OnTriggerEnter(Collider other)
     {
         craneEnter.Invoke();
+        isCrane = true;
+    }
+
+    private void Update()
+    {
+        if (!isCrane) return;
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (input == Vector2.zero) return;
+
+        CraneRotator();
+       
+        CabinaRotater();
+
+        JoysticRotate();
+        
+        Gruz.rotation = Quaternion.Euler(0f, 0f, 90);
+    }
+
+    private void CraneRotator()
+    {
+        var deltaRotation = input.y * rotateSpeed * Time.deltaTime;
+
+        currentXRotation =  Mathf.Clamp(currentXRotation + deltaRotation, craneRotX.x, craneRotX.y);
+        
+        cran.localRotation = Quaternion.Euler(currentXRotation, 0f, 90);
+    }
+
+    private void CabinaRotater()
+    {
+        var deltaRotation = input.x * rotateSpeed * Time.deltaTime;
+        
+        currentYRotation = Mathf.Clamp(currentYRotation + deltaRotation, craneRotY.x, craneRotY.y);
+        
+        cranePivot.rotation = Quaternion.Euler(0f, currentYRotation, 0f);
+    }
+
+    private void JoysticRotate()
+    {
+        if (joystic == null) return;
+        float tiltAroundX = input.y * 15;
+        float tiltAroundZ = -input.x * 15;
+        joystic.localEulerAngles = new Vector3(tiltAroundX, 0f, tiltAroundZ);
     }
 }
